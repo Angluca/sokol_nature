c4d2043380269322c24b96a5dd4d28eb9040339f
Pixeller <156404+Angluca@users.noreply.github.com>
2025-12-23 13:19:19 +0800

    sokol_nature

---
+++ README.md
@@ -0,0 +1,2 @@
+make getlib
+make
---
+++ lib/sokol_impl.c
@@ -0,0 +1,15 @@
+#define SOKOL_IMPL
+#define SOKOL_NO_ENTRY
+/*#define SOKOL_NO_METAL*/
+/*#define SOKOL_GLCORE*/
+
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_audio.h"
+#include "sokol_time.h"
+#include "sokol_args.h"
+#include "sokol_log.h"
+#include "sokol_fetch.h"
+#include "sokol_glue.h"
+/*#include "util/sokol_debugtext.h"*/
+
